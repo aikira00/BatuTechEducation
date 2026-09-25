b4703c2c99cd8eea21b126846759c7152576cd52
aikira00 <37047229+aikira00@users.noreply.github.com>
2026-09-25 12:31:34 +0200

    5 TPSI, PHP: esercizio 1 del 19/09

---
+++ 25-26_avoGit/5_tpsi/php/26_27_Nesi/091926_es1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>19 Settembre 2026 - Primo esercizio php</h1>
+    <?php
+        echo "ma che te lo dico a fare ?!? " . "</br>"
+    ?>
+    <form action="process.php" method="GET">
+        <input type="text" name="text"/>
+        <input type="submit" value="Click me!" />
+    </form>
+</body>
+</html>
---
+++ 25-26_avoGit/5_tpsi/php/26_27_Nesi/091926_es1/process.php
@@ -0,0 +1,4 @@
+<?php
+$text_name = $_GET["text"];
+echo "input text hai inserito: " . $text_name;
+?>
